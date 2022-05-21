66b5b0fd2b95ba3c4eeaaffa20a966172b0f2813
zzfima <zzfima@gmail.com>
2022-05-21 08:10:14 +0300

    big o

---
+++ BigO/BigO/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BigO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 50;
+
+            //f(n) = n, O(f(n)) = O(n)
+            for (int i = 0; i < n;)
+                i++;
+
+            //f(n) = n/3, O(f(n)) = O(n)
+            for (int i = 0; i < n;)
+                i += 3;
+
+            //f(n) = n*n, O(f(n)) = O(n^2)
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n;)
+                    j++;
+            }
+
+            //f(n) = n*(n/3), O(f(n)) = O(n^2)
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n;)
+                    j += 3;
+            }
+
+        }
+    }
+}
