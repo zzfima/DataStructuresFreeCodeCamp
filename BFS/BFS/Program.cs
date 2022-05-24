0aad0bf4559d276ed70c89e91aa51cd14c0ba1d8
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-05-24 14:00:43 +0300

    impl BFS

---
+++ BFS/BFS/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace BFS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create elements
+            TreeElement<int> elementA = new TreeElement<int>(1);
+            TreeElement<int> elementB = new TreeElement<int>(3);
+            TreeElement<int> elementC = new TreeElement<int>(5);
+            TreeElement<int> elementD = new TreeElement<int>(4);
+            TreeElement<int> elementE = new TreeElement<int>(7);
+            TreeElement<int> elementF = new TreeElement<int>(2);
+            TreeElement<int> elementG = new TreeElement<int>(11);
+
+            //connect elements
+            elementA.Neighbors.Add(elementB);
+
+            elementB.Neighbors.Add(elementA);
+            elementB.Neighbors.Add(elementC);
+            elementB.Neighbors.Add(elementD);
+            elementB.Neighbors.Add(elementE);
+
+            elementC.Neighbors.Add(elementB);
+            elementC.Neighbors.Add(elementE);
+
+            elementD.Neighbors.Add(elementB);
+            elementD.Neighbors.Add(elementF);
+
+            elementE.Neighbors.Add(elementB);
+            elementE.Neighbors.Add(elementC);
+            elementE.Neighbors.Add(elementF);
+            elementE.Neighbors.Add(elementG);
+
+            elementF.Neighbors.Add(elementD);
+            elementF.Neighbors.Add(elementE);
+
+            elementG.Neighbors.Add(elementE);
+
+            Queue<TreeElement<int>> queue = new Queue<TreeElement<int>>();
+            elementC.Level = 1;
+            queue.Enqueue(elementC);
+            TraverseTreeBFS.Traverse(queue);
+        }
+    }
+}
---
+++ BFS/BFS/TraverseTreeBFS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BFS
+{
+    static class TraverseTreeBFS
+    {
+        static int level = 0;
+
+        public static void Traverse(Queue<TreeElement<int>> elements)
+        {
+            level++;
+
+            Queue<TreeElement<int>> queue = new Queue<TreeElement<int>>();
+
+            foreach (var element in elements)
+            {
+                foreach (var neighbor in element.Neighbors)
+                {
+                    if (neighbor.Level == -1)
+                    {
+                        neighbor.Level = level + 1;
+                        Console.WriteLine($"value {neighbor.Value} at level {neighbor.Level}");
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+
+            if (queue.Count > 0)
+                Traverse(queue);
+        }
+    }
+}
