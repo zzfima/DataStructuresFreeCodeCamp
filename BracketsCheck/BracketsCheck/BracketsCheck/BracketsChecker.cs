ca13b6d25a5b5f02359c3d2b898730f50b688818
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-05-24 11:37:20 +0300

    add brackets checker by using stack

---
+++ BracketsCheck/BracketsCheck/BracketsCheck/BracketsChecker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BracketsCheck
+{
+    internal class BracketsChecker
+    {
+        public bool Check(char[] brackets)
+        {
+            Stack<char> bracketsStack = new Stack<char>();
+
+            foreach (var bracket in brackets)
+            {
+                //its left - insert it
+                if (IsLeftBracket(bracket))
+                    bracketsStack.Push(bracket);
+                //its right - pop and compare
+                else
+                {
+                    if (bracketsStack.Count == 0)
+                        return false;
+                    char b = bracketsStack.Pop();
+
+                    if (!IsComplementaryBrackets(b, bracket))
+                        return false;
+                }
+            }
+
+            if (bracketsStack.Count != 0)
+                return false;
+
+            return true;
+        }
+
+        private bool IsComplementaryBrackets(char leftBracket, char rightBracket)
+        {
+            if (leftBracket == '(' && rightBracket == ')')
+                return true;
+            if (leftBracket == '[' && rightBracket == ']')
+                return true;
+            return false;
+        }
+
+        private bool IsLeftBracket(char bracket)
+        {
+            if (bracket == '(' || bracket == '[')
+                return true;
+            return false;
+        }
+    }
+}
---
+++ BracketsCheck/BracketsCheck/BracketsCheck/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BracketsCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BracketsChecker bracketsChecker = new BracketsChecker();
+            Console.WriteLine(bracketsChecker.Check(new char[] { '(', '[', ']', '[', ']', ')' }));
+            Console.WriteLine(bracketsChecker.Check(new char[] { '(', '[', ']', '[', ']', ']' }));
+            Console.WriteLine(bracketsChecker.Check(new char[] { '(', '[', ']', '[', ']' }));
+        }
+    }
+}
