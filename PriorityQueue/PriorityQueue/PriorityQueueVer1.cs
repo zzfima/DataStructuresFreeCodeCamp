67120eb85e040149e468f0b8b2e80528f502c700
zzfima <zzfima@gmail.com>
2022-05-27 09:42:26 +0300

    rfct

---
+++ PriorityQueue/PriorityQueue/PriorityQueueVer1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PriorityQueue
+{
+    public class PriorityQueueVer1<T> where T : IComparable<T>
+    {
+        private readonly SortedSet<T> _queueArray;
+
+        public PriorityQueueVer1()
+        {
+            _queueArray = new SortedSet<T>();
+        }
+
+        public bool IsEmpty => _queueArray.Count == 0;
+
+        public void Enqueue(T v)
+        {
+            _queueArray.Add(v);
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty)
+                throw new Exception("PQ Is empty");
+
+            var v = _queueArray.Last();
+            _queueArray.Remove(v);
+            return v;
+        }
+    }
+}
---
+++ PriorityQueue/PriorityQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PriorityQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue1 = new PriorityQueueVer1<int>();
+
+            queue1.Enqueue(22);
+            queue1.Enqueue(12);
+            queue1.Enqueue(33);
+
+            while (queue1.IsEmpty == false)
+            {
+                Console.WriteLine(queue1.Dequeue());
+            }
+        }
+    }
+}
